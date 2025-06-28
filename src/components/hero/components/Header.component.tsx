2e8e31f8ca868902ad5dc643b3f9784a636d7b02
sarf01k <isaacksarfo1@gmail.com>
2025-06-28 12:26:05 +0000

    hero

--- src/components/hero/components/Header.component.tsx
+++ src/components/hero/components/Header.component.tsx
@@ -5,14 +5,14 @@ const navLinks = ["Product", "Customers", "Pricing", "Learn"];
 function Header() {
   return (
     <>
-      <header className="container grid grid-cols-2 my-6 bg-[#E9F3F4]">
+      <header className="container grid grid-cols-2 bg-[#E9F3F4]">
         <div className="flex justify-between items-center">
           <div className="flex gap-0.5 items-center">
             <img src={Logo} alt="logo" height={25} width={25} />
-            <span className="text-3xl font-black">Paylio</span>
+            <span className="text-4xl font-extrabold">Paylio</span>
           </div>
           <nav className="px-16">
-            <ul className="flex gap-12 text-sm font-medium items-center">
+            <ul className="flex gap-12 text-base font-semibold items-center">
               {navLinks.map((link) => {
                 return (
                   <li className="">
@@ -27,7 +27,7 @@ function Header() {
         <div className="flex justify-end items-center gap-2">
           <div>
             <a
-              className="border-1 border-gray-400 rounded-xl inline-flex items-center justify-center px-6 py-2 text-[#0B3E4E] text-sm font-medium"
+              className="border-1 border-[#b9bfc7] rounded-xl inline-flex items-center justify-center px-6 py-2 text-[#0B3E4E] text-base font-semibold"
               href=""
             >
               Login
@@ -35,7 +35,7 @@ function Header() {
           </div>
           <div>
             <a
-              className="border-1 border-[#2A8E9E] rounded-xl inline-flex items-center justify-center px-6 py-2 bg-[#2A8E9E] text-white text-sm font-medium"
+              className="border-1 border-[#2A8E9E] rounded-xl inline-flex items-center justify-center px-6 py-2 bg-[#2A8E9E] text-white text-base font-semibold"
               href=""
             >
               Sign Up
