a936bf44b91d148e4861e1d1eb6e09050841089c
mrgustaveh <mburuantony@outlook.com>
2025-11-30 01:28:48 +0300

    add features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -5,7 +5,7 @@ export const Features = () => {
   return (
     <div id="features">
       <p className="title">
-        Features <GoRocket className="features_icon" />
+        Features <GoRocket />
       </p>
 
       <div className="title-desc">
@@ -20,6 +20,46 @@ export const Features = () => {
           revolutionize your financial experience, from seamless transactions to
           investment opportunities.
         </p>
+      </div>
+
+      <div className="__features">
+        <div className="f_left">
+          <p className="f_title">Instant, Secure Payments</p>
+          <p className="f_desc">
+            Built on blockchain, we guarantee your peace of mind through secure
+            transfers at lightning speed with every transaction.
+          </p>
+
+          <div className="f_bg_image" />
+        </div>
+
+        <div className="f_right">
+          <div className="feature">
+            <span>
+              <p className="f_title">Multi-currency and Cross-Border Ready</p>
+              <p className="f_desc">
+                One platform, endless possibilities. Send, receive, pay and
+                invest globally without the borders.
+                <br />
+              </p>
+            </span>
+
+            <div className="f_bg_image crossborder" />
+          </div>
+
+          <div className="feature">
+            <span>
+              <p className="f_title">Smart Insights and Spending Analytics</p>
+              <p className="f_desc">
+                Let your data work for you, we help you keep track of your
+                investments and spending.
+                <br />
+              </p>
+            </span>
+
+            <div className="f_bg_image insights" />
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,38 +2,38 @@ import { MdStars } from "react-icons/md";
 import "../styles/components/header.scss";
 
 export const Header = () => {
-  return (
-    <div id="header">
-      <div className="l1">
-        <span className="welcome">
-          Welcome to Rift
-          <MdStars className="icon" />
-        </span>
+    return (
+        <div id="header">
+            <div className="l1">
+                <span className="welcome">
+                    Welcome to Rift
+                    <MdStars id="_icon" />
+                </span>
 
-        <p className="about">
-          Elevate your money,
-          <br />
-          Experience the
-          <br />
-          Future.
-        </p>
+                <p className="about">
+                    Elevate your money,
+                    <br />
+                    Experience the
+                    <br />
+                    Future.
+                </p>
 
-        <p className="info">
-          Rift transforms the way you pay, save and build your wealth
-          <br />
-          with a single account.
-        </p>
+                <p className="info">
+                    Rift transforms the way you pay, save and build your wealth
+                    <br />
+                    with a single account.
+                </p>
 
-        <a
-          href="https://wallet.riftfi.xyz/"
-          target="_blank"
-          className="getstrated"
-        >
-          Get Started
-        </a>
-      </div>
+                <a
+                    href="https://wallet.riftfi.xyz/"
+                    target="_blank"
+                    className="getstrated"
+                >
+                    Get Started
+                </a>
+            </div>
 
-      <div className="l2" />
-    </div>
-  );
+            <div className="l2" />
+        </div>
+    );
 };
