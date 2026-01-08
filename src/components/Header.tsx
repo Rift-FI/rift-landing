3aa43bd6dd9a20cfecfad6fd7b5a0341fd2d9b53
mrgustaveh <mburuantony@outlook.com>
2026-01-08 14:36:57 +0300

    minor fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,37 +3,39 @@ import { RotateText } from "./ui/animated-text";
 import "../styles/components/header.scss";
 
 export const Header = () => {
-    return (
-        <div id="header">
-            <div className="l1">
-                <span className="welcome">
-                    Welcome to Rift
-                    <MdStars id="_icon" />
-                </span>
+  return (
+    <div id="header">
+      <div className="l1">
+        <span className="welcome">
+          Welcome to Rift
+          <MdStars id="_icon" />
+        </span>
 
-                <p className="about">
-                    Elevate your money,
-                    <br />
-                    Experience the
-                    <br />
-                    Future.
-                </p>
+        <p className="about">
+          Elevate your money,
+          <br />
+          Experience the
+          <br />
+          Future.
+        </p>
 
-                <p className="info">
-                    Rift gives you direct secure access to world class yield generating assets <br />
-                    with a single USD account to <RotateText words={["Save.", "Pay.", "& Build your wealth."]} />
-                </p>
+        <p className="info">
+          Rift gives you direct secure access to world class yield generating
+          assets <br />
+          with a single USD account to{" "}
+          <RotateText words={["Save.", "Pay.", "Build your wealth."]} />
+        </p>
 
-                <a
-                    href="https://wallet.riftfi.xyz/"
-                    target="_blank"
-                    className="getstrated"
-                >
-                    Get Started
-                </a>
-            </div>
+        <a
+          href="https://wallet.riftfi.xyz/"
+          target="_blank"
+          className="getstrated"
+        >
+          Get Started
+        </a>
+      </div>
 
-            <div className="l2" />
-        </div>
-    );
+      <div className="l2" />
+    </div>
+  );
 };
